2e8c820a2e8de86d1d804990e72acbfde0c90503
rikka <1103670381@qq.com>
2021-07-31 22:55:34 +0800

    update: deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
  #!/bin/bash
 cd /var/hexo_source/hexo
 git pull>log.txt
-# hexo clean>>log.txt
-# hexo generate>>log.txt
-# gulp>>log.txt
+hexo clean>>log.txt
+hexo generate>>log.txt
+gulp>>log.txt
