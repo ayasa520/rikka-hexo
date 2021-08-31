fbe1dffd47e67aa2faf1f45cbd50eb6ce14eb81b
Rikka <1103670381@qq.com>
2021-08-31 21:44:00 +0800

    update

--- tempCodeRunnerFile.sh
+++
@@ -1,8 +0,0 @@
- cp -f -r public/* ayasa520.github.io 
-
-    cd ayasa520.github.io
-    git add .
-    git commit -m "更新样式"
-    # git push -f git@github.com:ayasa520/hexo.git master:gh-pages
-
-    git push 
